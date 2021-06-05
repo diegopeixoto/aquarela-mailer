bea06e9ad3b65a6dab7e06ed4a6d033076ef3151
Diego Peixoto <contato@diegopeixoto.com>
2021-06-05 02:28:52 -0300

    fix

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,5 +4,8 @@ import { MailerController } from '../controllers/MailerController'
 const mailer = new MailerController()
 const routes = Router()
 
-routes.use('/', mailer.send)
+routes.get('/', (req, res) => {
+    res.status(400).send('ahoy, wrong method ay')
+})
+routes.post('/', mailer.send)
 export { routes }
